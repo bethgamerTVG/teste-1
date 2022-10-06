5b6c68fc27954b3fac10077f6aac5146d4fea90c
Elizabeth Vitória <115173717+bethgamerTVG@users.noreply.github.com>
2022-10-06 16:04:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+lista = []
+resposta = 'sim'
+while resposta == 'sim':
+ nome = input('Digite um nome: ')
+ lista.append(nome)
+print(lista)
+resposta = input('Deseja digitar outro nome? ')
+os.system('cleara')
+
+print
